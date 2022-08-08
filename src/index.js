94c2aed35f69fae6e0714b8b2557ce4df98e4fbf
Oleg Gruntov <suppergruntov@gmil.com>
2022-08-08 23:03:49 +0300

    feat

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  let ans = [];
-  let res = [];
-  for(i= 0; i <= matrix.length; i++){
-if(i % 2 === 0){
-    ans.concat(matrix[i])
-}else{
-    ans.concat(matrix[i].reverse())
-}    
-  }
-  // for(key in ans){
-  //   res = res.concat([ans[key]]);
-  // }
-  return ans;
+module.exports = function towelSort(matrix) {
+    let ans = [];
+    if (typeof matrix == "undefined") {
+        return [];
+    } else {
+        for (i = 0; i <= matrix.length; i++) {
+            if (i % 2 === 0) {
+             ans.concat(matrix[i])
+            } else {
+             ans.concat(matrix[i].reverse())
+            }
+        }
+    }
+        return ans;
 }
