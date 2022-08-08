815b24a229aca07ed8558384008509f1902f1aa3
Oleg Gruntov <suppergruntov@gmil.com>
2022-08-08 23:14:41 +0300

    feat:

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@
 
 module.exports = function towelSort(matrix) {
     let ans = [];
+    let ret = [];
     if (typeof matrix == "undefined") {
         return [];
     } else {
-        for (i = 0; i <= matrix.length; i++) {
+        for (i = 0; i < matrix.length; i++) {
             if (i % 2 === 0) {
-             ans.concat(matrix[i])
+             ans.push(matrix[i])
             } else {
-             ans.concat(matrix[i].reverse())
+             ans.push(matrix[i].reverse())
             }
         }
+    };
+    for(let k in ans ){
+     ret =  ret.concat(ans[k])
     }
-        return ans;
+        return ret;
 }
